b7f02e21296daef84e0fb125129aba92035edb69
Andy Stark <andy.stark@contractor.alfresco.com>
2018-03-05 16:43:11 +0000

    Added stoplist

--- docs/document-list.component.md
+++ docs/document-list.component.md
@@ -1,7 +1,7 @@
 ---
 Added: v2.0.0
 Status: Active
-Reviewed: 2017-11-27
+Last reviewed: 2018-01-01
 ---
 # Document List component
 
--- lib/config/DocProcessor/githubGraphTest.ts
+++ lib/config/DocProcessor/githubGraphTest.ts
@@ -1,12 +1,26 @@
 import * as path from "path";
+import * as fs from "fs";
 
 import { GraphQLClient } from "graphql-request";
+import * as remark from "remark";
+import * as frontMatter from "remark-frontmatter";
+import * as yaml from "js-yaml";
+import * as moment from "moment";
+
 import * as process from "process"
 import * as libsearch from "./libsearch";
+import { Stoplist } from "./stoplist";
 
+
+const adf20StartDate = "2017-11-20";
 
 const rootFolder = ".";
-var srcData = {};
+const stoplistFilePath = path.resolve("config", "DocProcessor", "commitStoplist.json");
+
+let srcData = {};
+let stoplist = new Stoplist(stoplistFilePath);
+
+let docsFolderPath = path.resolve("..", "docs");
 
 console.log("Compiling list of component sources...");
 libsearch(srcData, path.resolve(rootFolder));
@@ -44,16 +58,42 @@ const query = `query commitHistory($path: String) {
   }
 }`;
 
+let key = "document-list.component";
 
 let vars = {
-  "path": "lib/" + srcData["document-list.component"].path
+  "path": "lib/" + srcData[key].path
 };
 
 client.request(query, vars).then(data => {
     let nodes = data["repository"].ref.target.history.nodes;
 
+    let mdFilePath = path.resolve(docsFolderPath, key + ".md");
+
+    let mdText = fs.readFileSync(mdFilePath);
+    let tree = remark().use(frontMatter, ["yaml"]).parse(mdText);
+
+    let lastReviewDate = moment(adf20StartDate);
+
+    if (tree.children[0].type == "yaml") {
+      let metadata = yaml.load(tree.children[0].value);
+      lastReviewDate = moment(metadata["Last reviewed"]);
+    }
+
+    let numUsefulCommits = 0;
+
     nodes.forEach(element => {
-      let abbr = element.message.substr(0, 15);
-      console.log(`${element.pushedDate}: ${abbr}`);
+      if (!stoplist.isRejected(element.message)) {
+        let abbr = element.message.substr(0, 15);
+        console.log(`${element.pushedDate}: ${abbr}`);
+
+        let commitDate = moment(element.pushedDate);
+
+        if (commitDate.isAfter(lastReviewDate)) {
+          numUsefulCommits++;
+        }
+      }
     });
+
+    console.log("Commits since review:" + numUsefulCommits);
 });
+
